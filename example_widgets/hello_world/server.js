a7a657ac1f4c6aa824a617b19c7ef0a0ad8894ca
Strauch <nstrauch@deloitte.de>
2020-04-01 14:18:46 +0200

    hello world changes

---
+++ example_widgets/hello_world/server.js
@@ -0,0 +1,14 @@
+(function() {
+    /* populate the 'data' object */
+    /* e.g., data.table = $sp.getValue('table'); */
+      data.incident = [];
+      var myIncident = new GlideRecord('incident');
+      myIncident.addQuery('active', true);
+      myIncident.setLimit(10);
+      myIncident.query();
+      while(myIncident.next()){
+          data.incident.push(myIncident.getValue('number'));
+      }
+      
+  
+  })();
